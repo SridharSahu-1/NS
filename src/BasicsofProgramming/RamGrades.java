77382b6fc7f7aa68bebe2078edef4849b472428d
Sridhar Sahu <sridharsahu5555@gmail.com>
2022-08-28 15:57:06 +0530

    NS

---
+++ src/BasicsofProgramming/RamGrades.java
@@ -0,0 +1,18 @@
+package BasicsofProgramming;
+
+import java.util.Scanner;
+
+public class RamGrades {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float a = sc.nextFloat();
+        float b = sc.nextFloat();
+        float c = sc.nextFloat();
+        float d = sc.nextFloat();
+        float totalmarks = a+b+c+d;
+        float percentage = totalmarks/400*100;
+        System.out.println((int)percentage);
+
+
+    }
+}
