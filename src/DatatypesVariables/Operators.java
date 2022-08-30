3b1e091da0546cb8f972f9c4d2d9402cbdff8de7
Sridhar Sahu <sridharsahu5555@gmail.com>
2022-08-30 20:23:52 +0530

    NS Assignments

---
+++ src/DatatypesVariables/Operators.java
@@ -0,0 +1,16 @@
+package DatatypesVariables;
+
+import java.util.Scanner;
+
+public class Operators {
+    public static void main (String[] args) {
+        // Your code here
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b= sc.nextInt();
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+    }
+}
