aaef44040d802ea5e6278f2d11db7495d01e44ce
Sridhar Sahu <sridharsahu5555@gmail.com>
2022-09-02 21:32:53 +0530

    NS Assignments

---
+++ src/DatatypesVariables/SimpleInputString.java
@@ -0,0 +1,13 @@
+package DatatypesVariables;
+
+import java.util.Scanner;
+
+public class SimpleInputString {
+    public static void main (String[] args) {
+        // Your code here
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.println(str);
+
+    }
+}
