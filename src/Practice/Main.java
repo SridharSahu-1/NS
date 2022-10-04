7c28aa2d2bbb009baf58db0b61ddc6a152a95cf6
Sridhar Sahu <sridharsahu5555@gmail.com>
2022-10-04 16:41:26 +0530

    Added Hashing Assignments

---
+++ src/Practice/Main.java
@@ -0,0 +1,16 @@
+package Practice;
+
+public class Main {
+    public static void main(String[] args) {
+        LLPract ll = new LLPract();
+        ll.addFirst(2);
+        ll.addFirst(1);
+        ll.addFirst(3);
+        ll.addFirst(5);
+        ll.addLast(433);
+        ll.addLast(21);
+        ll.addLast(65);
+        ll.addLast(76543);
+        ll.display();
+    }
+}
